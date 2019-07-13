e19ea2a6b3d0920b1292f4dc9ab823266698e674
wishajaveria <49516832+wishajaveria@users.noreply.github.com>
2019-07-13 12:41:23 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,1340 @@
+
+let techkaro = [{
+    facilities: ["convence", "internet", "air conditioned classes"]
+
+},
+{
+    lifeskills: ["confidence", "interview", "body language", "voice councils", "speaking"]
+},
+{
+    classtiming: "4 hours"
+},
+{
+    totalcomputers: 500
+},
+{
+    totalstudents: 480
+},
+{
+    careempakage: "50% of"
+},
+{
+    internships: "3 months"
+}
+]
+
+let expenses = [{
+    websiteURL:"50000",
+    classresources:"40000",
+    slackURL:"8000",
+    github:"700000",
+
+}]
+
+/*about:"to learn web development our students",
+facilities:["convence","air conditioned room","internet","computers" ],
+internships:"",
+mentormeetups:"",
+timmming:"2:00 to 6:00",
+totalcomputers:500,
+totalstudents:450,
+careempakage:"50%0f",
+backsuppoters:"",
+
+
+
+
+
+
+Time management. ",      
+    skill2: "Critical thinking and problem-solving",
+    skill3:"Agility and adaptability.",
+    skill4:"Communication skills.",
+    skill5:"Problem solving and logical thinking",
+    skill6:"confident",
+    skill7:"teamWork",
+    skill8:"self-study",
+    
+*/
+
+
+
+let students = [{
+    name: "yusra",
+    gardian: "father",
+    contact: "0323256790977",
+    gender: "female",
+    attendence:"60%",
+    fees: 1000,
+    batch:"2019",
+    coursename: "web development",
+    duration: 8,
+    schedule: "tough",
+    certifications: 4,
+    ispractice: "yes",
+    issmartphone: "yes",
+    iscomputer: "no",
+    unavailability: "2 months",
+    practicehours: "2hours",
+    email: "yusra.raza@gmail.com",
+    skills: ["communication skill", "teamwork", "taking decisions"],
+    isinterest: "yes",
+    qualification: "intermediate",
+    background: "computer science",
+    isonjob: "no",
+    distanceinkm: "5km",
+    isgithub: "yes",
+    feedback: ["facilities", "extra practice hours"],
+    successstories: "more success stories",
+    employ: {
+        hasjob: "no",
+        timing: "3 hours",
+        salary: "no",
+        distanceinkm: "5km",
+        iscommingdirectilfromjob: "no",
+        institute: "gulshan college",
+    }
+
+},
+{
+    name: "fatima",
+    gardian: "father",
+    contact: "032532739469",
+    fees: 500,
+    batch:"2019",
+    attendence:"80%",
+    coursename: "web development",
+    duration: 6,
+    schedule: "",
+    certifications: 2,
+    ispractice: "yes",
+    issmartphone: "no",
+    iscomputer: "yes",
+    unavailability: "2 months",
+    practicehours: "3 hours",
+    email: "fatima.5765@gmail.com",
+    skills: ["self study", "confidence", "taking decisions"],
+    isinterest: "yes",
+    qualification: "B.com",
+    background: "commerce",
+    isonjob: "yes",
+    distanceinkm: "8km",
+    isgithub: "yes",
+    feedback: ["convence facility", "extra practice hours"],
+    successstories: "none",
+    employ: {
+        hasjob: "yes",
+        timing: "6 hours",
+        salary: 10000,
+        distanceinkm: "8km",
+        iscommingdirectilfromjob: "yes",
+        institute: "al habib bank",
+    },
+},
+{
+    name: "hania",
+    gardian: "father",
+    contact: "036458366987",
+    fees: 700,
+    batch:"2019",
+    attendence:"40%",
+    coursename: "web development",
+    duration: 4,
+    schedule: "tough",
+    certifications: 6,
+    ispractice: "yes",
+    issmartphone: "yes",
+    iscomputer: "yes",
+    unavailability: "4 months",
+    practicehours: "10 hours",
+    email: "hania.ahmed@gmail.com",
+    skills: ["self study", "teamwork",],
+    isinterest: "yes",
+    qualification: "software engineer",
+    background: "computer science",
+    isonjob: "yes",
+    distanceinkm: "6km",
+    isgithub: "no",
+    feedback: ["extra practice hours"],
+    successstories: "none",
+    employ: {
+        hasjob: "yes",
+        timing: "5hours",
+        salary: 20000,
+        distanceinkm: "6km",
+        iscommingdirectilfromjob: "yes",
+        institute: "steel meals company",
+    },
+},
+{
+    name: "maheen",
+    gardian: " grand father",
+    contact: "035378698555",
+    fees: 400,
+    batch:"2019",
+    gender:"female",
+    attendence:"50%",
+    coursename: "web development",
+    duration: 3,
+    schedule: "tough",
+    certifications: 2,
+    ispractice: "yes",
+    issmartphone: "no",
+    iscomputer: "no",
+    unavailability: "2 months",
+    practicehours: "3 hours",
+    email: "maheen.767@gmail.com",
+    skills: ["communication skills", "self study",],
+    isinterest: "no",
+    qualification: "intermediate",
+    background: "arts",
+    isonjob: "yes",
+    distanceinkm: "9km",
+    isgithub: "yes",
+    feedback: ["extra pratice time"],
+    successstories: "none",
+    employ: {
+        hasjob: "yes",
+        timing: "8hours",
+        salary: 8000,
+        distanceinkm: "9km",
+        iscommingdirectilfromjob: "yes",
+        institute: "iqra school",
+    },
+},
+{
+    name: "kawal",
+    gardian: " grand father",
+    contact: "0353474788",
+    fees: 400,
+    batch:"2019",
+    gender:"female",
+    attendence:"90%",
+    coursename: "web development",
+    duration: 5,
+    schedule: "tough",
+    certifications: 6,
+    ispractice: "yes",
+    issmartphone: "no",
+    iscomputer: "yes",
+    unavailability: "4 months",
+    practicehours: "6 hours",
+    email: "kawal.77@gmail.com",
+    skills: ["hardworker", "self study",],
+    isinterest: "yes",
+    qualification: "software engineer",
+    background: "somputer science",
+    isonjob: "yes",
+    distanceinkm: "9km",
+    isgithub: "no",
+    feedback: ["extra pratice time"],
+    successstories: "none",
+    employ: {
+        hasjob: "yes",
+        timing: "4hours",
+        salary: 10000,
+        distanceinkm: "9km",
+        iscommingdirectilfromjob: "yes",
+        institute: "expert web design company",
+    },
+},
+{
+    name: "rehman",
+    gardian: "  father",
+    contact: "03765876523",
+    fees: 500,
+    batch:"2019",
+    gender:"male",
+    attendence:"70%",
+    coursename: "web development",
+    duration: 5,
+    schedule: "tough",
+    certifications: 7,
+    ispractice: "yes",
+    issmartphone: "yes",
+    iscomputer: "no",
+    unavailability: "none",
+    practicehours: "9 hours",
+    email: "rehman.khan@gmail.com",
+    skills: ["confidence", "self study",],
+    isinterest: "yes",
+    qualification: "B.com",
+    background: "commerce",
+    isonjob: "yes",
+    distanceinkm: "9km",
+    isgithub: "no",
+    feedback: ["extra pratice time" ,"convence facility"],
+    successstories: "none",
+    employ: {
+        hasjob: "yes",
+        timing: "8hours",
+        salary: 13000,
+        distanceinkm: "9km",
+        iscommingdirectilfromjob: "yes",
+        institute: "daraz.pk",
+    },
+},
+{
+    name: "usman",
+    gardian: "  father",
+    contact: "03848973354",
+    fees: 400,
+    batch:"2019",
+    gender:"male",
+    attendence:"50%",
+    coursename: "web development",
+    duration: 7,
+    schedule: "tough",
+    certifications: 7,
+    ispractice: "yes",
+    issmartphone: "yes",
+    iscomputer: "yes",
+    unavailability: "1 months",
+    practicehours: "7 hours",
+    email: "usman.zahid@gmail.com",
+    skills: ["communication skills", "harworker",],
+    isinterest: "yes",
+    qualification: "chemical engineer",
+    background: "pre-engineer",
+    isonjob: "yes",
+    distanceinkm: "10km",
+    isgithub: "no",
+    feedback: "none",
+    successstories: "none",
+    employ: {
+        hasjob: "yes",
+        timing: "8hours",
+        salary:17000,
+        distanceinkm: "9km",
+        iscommingdirectilfromjob: "yes",
+        institute: "jo bolo ",
+    },
+},
+{
+    name: "ahmed",
+    gardian: "  father",
+    contact: "0349387755",
+    fees: 500,
+    batch:"2019",
+    gender:"male",
+    attendence:"80%",
+    coursename: "web development",
+    duration: 4,
+    schedule: "tough",
+    certifications: 9,
+    ispractice: "yes",
+    issmartphone: "no",
+    iscomputer: "yes",
+    unavailability: "1 months",
+    practicehours: "3 hours",
+    email: "ahmed.jamal@gmail.com",
+    skills: ["confidence", "self study",],
+    isinterest: "yes",
+    qualification: "b.sc",
+    background: "commerce",
+    isonjob: "yes",
+    distanceinkm: "9km",
+    isgithub: "no",
+    feedback: ["extra pratice time", "good teachres"],
+    successstories: "none",
+    employ: {
+        hasjob: "yes",
+        timing: "8hours",
+        salary: 10000,
+        distanceinkm: "5km",
+        iscommingdirectilfromjob: "yes",
+        institute: "steell meal company",
+    },
+}]
+
+let staff = [{
+    name: "sir raza",
+    position: "",
+    qualification: "sofware engineer",
+    experience: "2 years experince in a web design company",
+    about: "",
+    batch:"2019",
+    gender: "male",
+    email: "raza,8@gamil.com",
+    phone: "032553294758",
+    github: "raza.ahmed",
+    skills: ["web development", " photoshop"],
+    issmartphone: "yes",
+    profession: "employ",
+    practicehours: "5 hours",
+    unavailability: "none",
+
+},
+{
+    name: "miss amarah",
+    position: "",
+    qualification: "electrical  engineer",
+    experience: "4 years experince in a web design company",
+    about: "",
+    gender: "female",
+    email: "amarah.56@gamil.com",
+    phone: "032553294758",
+    github: "amarah.asif",
+    skills: ["web development", " web designing"],
+    issmartphone: "no",
+    profession: "teaching",
+    practicehours: "3 hours",
+    unavailability: "2 months of july",
+
+},
+{
+    name: "sir raza",
+    position: "",
+    qualification: "sofware engineer",
+    experience: "2 years experince in a web design company",
+    about: "",
+    gender: "male",
+    email: "raza,8@gamil.com",
+    phone: "032553294758",
+    batch:"2019",
+    github: "raza.ahmed",
+    skills: ["web development", " photoshop"],
+    issmartphone: "yes",
+    profession: "teaching",
+    practicehours: "5 hours",
+    unavailability: "none",
+
+},
+{
+    name: "sir kamran",
+    position: "normal",
+    qualification: "sofware engineer",
+    experience: "2 years experince in a web design company",
+    about: "good teacher",
+    gender: "male",
+    email: "kamaran.faiz@gamil.com",
+    batch:"2019",
+    phone: "03275683768",
+    github: "raza.ahmed",
+    skills: ["web development", " word press"],
+    issmartphone: "yes",
+    profession: "teaching",
+    practicehours: "6 hours",
+    unavailability: "none",
+},
+{
+    name: "sir umer",
+    position: "normal",
+    qualification: "electrical engineer",
+    experience: "1 years experince in a web design company",
+    about: "none",
+    batch:"2019",
+    gender: "male",
+    email: "umer.8@gamil.com",
+    phone: "037686875589",
+    github: "umer asif",
+    skills: [ " photoshop"],
+    issmartphone: "yes",
+    profession: "employ",
+    practicehours: "5 hours",
+    unavailability: "4 months",
+},
+{
+    name: "mis aleena",
+    position: "good",
+    qualification: "sofware engineer",
+    experience: "3 years experince in a web design company",
+    about: "none",
+    batch:"2018",
+    gender: "female",
+    email: "aleena.akmal@gamil.com",
+    phone: "0353478975943",
+    github: "aleena akmal",
+    skills: ["web development", "react"],
+    issmartphone: "yes",
+    profession: "employ",
+    practicehours: "8 hours",
+    unavailability: "2 weeks",
+},
+{
+    name: "miss maria",
+    position: "normal",
+    qualification: "maters in mathematics",
+    experience: "none",
+    about: "none",
+    batch:"2019",
+    gender: "female",
+    email: "maria.shadid@gamil.com",
+    phone: "8357394579759",
+    github: "maria shahid",
+    skills: [" javascript", "photoshop"],
+    issmartphone: "no",
+    profession: "none",
+    practicehours: "7 hours",
+    unavailability: "7 months",
+},
+{
+    name: "sir muneeb",
+    position: "normal",
+    qualification: "B.com",
+    experience: "5 year",
+    about: "none",
+    batch:"2018",
+    gender: "male",
+    email: "muneeb.raza@gamil.com",
+    phone: "0374537898",
+    github: "muneeb raza",
+    skills: ["web development", " word press"],
+    issmartphone: "no",
+    profession: "employ",
+    practicehours: "5 hours",
+    unavailability: "none",
+}]
+let events = ["life skills","mentor metups" ,"meeting with circle","ned tour" ]
+
+let teachers = [{
+    name: " miss zunaira",
+    position: "normal",
+    qualification: "chemical engineer",
+    experience: "3 years experince in a web design company",
+    about: "",
+    batch:"2019",
+    gender: "female",
+    email: "zunaira.shah@gamil.com",
+    phone: "037045705687",
+    github: "zunaira shah",
+    skills: ["web development", " photoshop"],
+    issmartphone: "yes",
+    profession: "employ",
+    practicehours: "5 hours",
+    unavailability: "2 months",
+
+},
+{
+    name: "miss saba",
+    position: "normal",
+    qualification: "electrical  engineer",
+    experience: "4 years experince in a web design company",
+    about: "",
+    gender: "female",
+    email: "saba.sohail@gamil.com",
+    phone: "07659423466",
+    github: "saba sohail",
+    skills: ["self study", " web designing"],
+    issmartphone: "no",
+    profession: "as a employ",
+    practicehours: "3 hours",
+    unavailability: "2 months of july",
+
+},
+{
+    name: "sir ajmal",
+    position: "good",
+    qualification: "sofware engineer",
+    experience: "5 years experince in a web design company",
+    about: "none",
+    gender: "male",
+    email: "ajmal.khan@gamil.com",
+    phone: "03865935648",
+    batch:"2013",
+    github: "ajmal khan",
+    skills: ["web development", " react ","photo shop"],
+    issmartphone: "yes",
+    profession: "employ",
+    practicehours: "5 hours",
+    unavailability: "none",
+
+},
+{
+    name: "mam nadia",
+    position: "normal",
+    qualification: "electrical engineer",
+    experience: "3 years experince in a web design company",
+    about: "good teacher",
+    gender: "female",
+    email: "nadia.faraz@gamil.com",
+    batch:"2019",
+    phone: "0237569375930",
+    github: "nada faraz",
+    skills: ["team work"," word press"],
+    issmartphone: "yes",
+    profession: "teaching",
+    practicehours: "6 hours",
+    unavailability: "none",
+}]
+
+
+function print() {
+    console.log(students);
+    console.log(staff);
+    console.log(techkaro);
+    console.log(expenses);
+    console.log(events);
+    console.log(teacher);
+}
+
+let management = ["sadaf abid" ,"tooba","mashal"]
+
+let  ambassador = [
+    {
+        name:"miss noreen",
+        ishardworker:"yes",
+        age:23,
+        isonjob:"no",
+        gender:"female",
+        distanceinkm:"4km",
+        isgithub:"yes",
+        isslack:"yes",
+        issamrtphone:"yes",
+        email:"noreen.65@gmail.com",
+        dutytiming:"4 hours",
+        skills:["self learning" ,"javascript"],
+        salary:10000,
+        passion:"80%",
+        iscomputer:"yes",
+        certifications:8,
+        experience:"1 year",
+        unavailability:"2 months"
+
+    },
+    {
+
+        name:"sir ali",
+        ishardworker:"yes",
+        age:24,
+        isonjob:"yes",
+        gender:"male",
+        distanceinkm:"3km",
+        isgithub:"yes",
+        isslack:"yes",
+        issamrtphone:"yes",
+        email:"ali.645@gmail.com",
+        dutytiming:"4 hours",
+        skills:["confidence" ,"javascript"],
+        salary:12000,
+        passion:"85%",
+        iscomputer:"yes",
+        certifications:6,
+        experience:"2 year",
+        unavailability:"3 months"
+    },
+{
+    name:"miss abira",
+        ishardworker:"yes",
+        age:24,
+        isonjob:"no",
+        gender:"female",
+        distanceinkm:"6km",
+        isgithub:"yes",
+        isslack:"no",
+        issamrtphone:"yes",
+        email:"abira.88@gmail.com",
+        dutytiming:"4 hours",
+        skills:["self learning" ,"wordpress"],
+        salary:10000,
+        passion:"70%",
+        iscomputer:"no",
+        certifications:8,
+        experience:" no",
+        unavailability:"none"
+},
+{
+    name:"miss rabia",
+        ishardworker:"yes",
+        age:23,
+        isonjob:"yes",
+        gender:"female",
+        distanceinkm:"8km",
+        isgithub:"no",
+        isslack:"yes",
+        issamrtphone:"yes",
+        email:"rabia.443@gmail.com",
+        dutytiming:"4 hours",
+        skills:["teamwork" ,"javascript"],
+        salary:10000,
+        passion:"86%",
+        iscomputer:"yes",
+        certifications:6,
+        experience:"2 year",
+        unavailability:"none",
+},
+{
+    name:"sir waleed",
+        ishardworker:"yes",
+        age:24,
+        isonjob:"yes",
+        gender:"male",
+        distanceinkm:"5km",
+        isgithub:"yes",
+        isslack:"yes",
+        issamrtphone:"no",
+        email:"waleed.765@gmail.com",
+        dutytiming:"4 hours",
+        skills:["self learning" ,"teamwork"],
+        salary:10000,
+        passion:"75%",
+        iscomputer:"yes",
+        certifications:5,
+        experience:"1 year",
+        unavailability:"4 months"
+}]
+
+let lifeskills = [{
+    class1:"cv class",
+    teacher:"sir tahir",
+    classtiming:"3hours"
+},
+{
+    class2:"interview class",
+    teacher:"sir tahir",
+    classtiming:"4hours"
+},
+{
+    class3:"body lanuage class",
+    teacher:["sir yaseen ", "sir sunain"],
+    classtiming:"4hours"
+},
+{
+    class4:"word press class",
+    teacher:"sir ali",
+    classtiming:"3hours"
+
+},
+{
+    class5:"confidence build up class",
+    teacher:"sir amar",
+    classtiming:"2hours",
+}]
+
+let webdevelopmenttopics = [{
+    javascript:["loops","array","ifelse","dta types","comments" ]
+},
+{
+    react:["react render html","rect state","react life cycle"]
+},
+{
+    jason:["data types", "operators","syntax","comments"]
+}] 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+777777777777777777777777777777777777777777777777777777777
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
